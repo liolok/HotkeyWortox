627dd787b3ca0f088555e007d96909f841385789
liolok <i@liolok.com>
2025-05-18 23:39:09 +0800

    refactor modmain

--- modmain.lua
+++ modmain.lua
@@ -1,32 +1,21 @@
-PrefabFiles = { 'blink_marker' }
 modimport('keybind')
-local G = GLOBAL
-local fn = require('liolok_hotkey/wortox')
+
+local Input = GLOBAL.TheInput
 local handler = {} -- config name to key event handlers
+local fn = require('liolok_hotkey/wortox')
+
+PrefabFiles = { 'blink_marker' } -- Soul Hop target position display | 灵魂跳跃目标位置显示
+AddComponentPostInit('playercontroller', fn.RefreshBlinkMarkers)
 
 function KeyBind(name, key)
   -- disable old binding
-  if handler[name] then
-    handler[name]:Remove()
-    handler[name] = nil
-  end
+  if handler[name] then handler[name]:Remove() end
 
-  -- if has binding, will display its blink marker
+  -- to display target position only if these two hotkeys have binding
   if name == 'BlinkToEntity' then TUNING.HOTKEY_WORTOX_ENTITY = key ~= nil end
   if name == 'BlinkToMostFar' then TUNING.HOTKEY_WORTOX_FURTHEST = key ~= nil end
 
-  -- no binding
-  if not key then return end
-
   -- new binding
-  if key >= 1000 then -- it's a mouse button
-    handler[name] = G.TheInput:AddMouseButtonHandler(
-      function(button, down) return (button == key and down and fn.IsPlaying('wortox')) and fn[name]() end
-    )
-  else -- it's a keyboard key
-    handler[name] = G.TheInput:AddKeyDownHandler(key, function() return fn.IsPlaying('wortox') and fn[name]() end)
-  end
+  local function f(_key, down) return (_key == key and down and fn.IsPlaying('wortox')) and fn[name]() end
+  handler[name] = key and (key >= 1000 and Input:AddMouseButtonHandler(f) or Input:AddKeyHandler(f))
 end
-
--- Soul Hop Target Position Display | 灵魂跳跃目标位置显示
-AddComponentPostInit('playercontroller', fn.RefreshBlinkMarkers)
